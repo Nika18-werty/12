7434feec91a406e1e31961e5c9da004aec07ede4
mka <mka@A26PC7>
2024-11-11 20:14:07 +0200

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "Student.h"
+#include "Aspirant.h"
+
+using namespace std;
+
+int main()
+{
+
+    Student student("Іван Іванович", 20, "Київський університет", "Право");
+    student.displayInfo();
+
+    cout << endl;
+
+    Aspirant aspirant("Вероніка Демчук", 24, "Київський університет", "Інформатика", "Розробка програмного забезпечення");
+    aspirant.displayInfo();
+
+    cout << "\n\n"
+}
