ca5e3a3993bb62de76781a6a617b1dc3eb617216
Nika <nika@gmail.com>
2024-11-11 20:40:55 +0200

    task 2

---
+++ dz2/dz2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "ForeignPassport.h"
+
+using namespace std;
+
+int main()
+{
+
+    Passport passport("Вероніка", "Демчук", "18.10.2000", "ID12345678", "01.01.2020", "01.01.2050");
+
+    passport.printInfo();
+
+    cout << endl;
+
+    ForeignPassport foreignPassport("Вероніка", "Демчук", "18.10.2000", "SD678543", "01.01.2020", "01.01.2050",
+        "FM12345678", "Дипломатична віза до 2050");
+
+    foreignPassport.printInfo();
+
+}
